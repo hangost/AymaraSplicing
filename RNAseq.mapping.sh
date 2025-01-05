cb125a38062f2bedb0c14fc9d01d28eaf5f8286c
hangost <u6008737@utah.edu>
2025-01-06 08:07:28 +0900

    Create RNAseq.mapping.sh

---
+++ RNAseq.mapping.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --time=72:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=12
+#SBATCH -o ./tmp/STAR.o.%j
+#SBATCH --err ./tmp/STAR.e.%j
+
+ml R/3.3.2
+ml samtools
+ml star/2.5
+sampleNM=${1##*/}
+sampleNM=${sampleNM%%_*}
+Foutdir=${4}/${sampleNM}/
+
+echo ${Foutdir}
+mkdir -p ${Foutdir}
+
+STAR --genomeDir ./STAR-genome2/STAR/Total/ --sjdbFileChrStartEnd ./STAR-genome2/STAR/Total/sjdbList.fromGTF.out.tab --runThreadN $3 --outFileNamePrefix ${Foutdir} --outSAMtype BAM SortedByCoordinate --readFilesIn $1 $2
